55fe103d00debc96478b0668b3a9f8383922339f
mohitgajjar1811 <mohitgajjar9535@gmail.com>
2026-04-28 23:56:19 +0530

    about us

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -183,7 +183,7 @@
                     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="rounded shadow overflow-hidden">
                             <div class="position-relative">
-                                <img class="img-fluid" src="{{ asset('storage/' . $i->image) }}" alt="">
+                                <img class="img-fluid" src="{{ asset('img_uploads/' . $i->image) }}" alt="">
 <!--                                <div class="position-absolute start-50 top-100 translate-middle d-flex align-items-center">-->
 <!--                                    <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>-->
 <!--                                    <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>-->
@@ -193,6 +193,9 @@
                             <div class="text-center p-4 mt-3">
                                 <h5 class="fw-bold mb-0">{{ $i->name }}</h5>
                                 <small>{{ $i->dsg }}</small>
+                                @if($i->description)
+                                    <p class="text-muted mt-2 mb-0" style="font-size: 14px;">{{ $i->description }}</p>
+                                @endif
                             </div>
                         </div>
                     </div>
